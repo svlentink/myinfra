b4fc1e37eecd8c61314701ba762c0d13c73f9468
SanderLentink.nl <sander@lent.ink>
2020-06-17 18:16:25 +0000

    proper reset script to delete volumes as well

--- reset.sh
+++ reset.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+df -h|grep '/$'
+for i in k8sconf/*-pod-*.yml;do
+  kubectl delete -f $i
+done
+for i in k8sconf/*-pvc-*.yml;do
+  kubectl delete -f $i
+done
+for i in k8sconf/*-pv-*.yml;do
+  kubectl delete -f $i
+done
 microk8s.reset
+df -h|grep '/$'
 microk8s.enable ingress storage dns
 kubectl apply --kustomize ~/.sekretoj
 for i in k8sconf/*.yml;do
