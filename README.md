857841efb5cac653327d732453205a2251c1e5ab
Sander Lentink <sander@lent.ink>
2020-10-24 19:24:38 +0200

    removed documentation and created script instead for backing up dirs

--- README.md
+++ README.md
@@ -10,16 +10,6 @@ This repo. contains my current setup using kubernetes.
 At this moment I only run services on port 80, 443, 443 udp and 53 udp.
 
 
-## Mounted directories
-
-<!--
-~/.ssh keys
--->
-
-```shell
-for f in `find . -name '*.yml'`;do grep 'path: /' $f|grep -v '\- path' && echo $f; done
-```
-
 ## FIXME
 
 - explain life cycle management (update cadence)
---
+++ scripts/backup-microk8s.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if kubectl get pvc -A|grep -i bound; then
+  read -n1 -p "Still volumes mounted, this can affect DBs, are you sure?[Y/n]" YN
+  if [[ "$YN" == [Yy] ]]; then
+    echo " OK continuing"
+  else
+    echo " Aborting"
+    exit 1
+  fi
+fi
+
+SEARCH_DIR=/mnt/k8s/ide/gits/myinfra/namespaces
+OTHER_DIRS="$HOME/.ssh"
+
+PATHS=$(for f in `find $SEARCH_DIR -name '*.yml'`;do grep '^[\t\ ]*path: /' $f|grep -o "/.*"|grep -v '^/tmp'; done|sort)
+
+PREVIOUS_PATH="UNSET"
+for p in $PATHS;do
+  if [[ "$p" == "$PREVIOUS_PATH"* ]]; then
+    #echo "$p part of $PREVIOUS_PATH"
+    true
+  else
+    PREVIOUS_PATH="$p"
+    ROOT_PATHS="$p $ROOT_PATHS"
+  fi
+done
+
+set -v
+tar czf "/tmp/backup-`hostname`-`date +%F`.tgz" $ROOT_PATHS $OTHER_DIRS
+
