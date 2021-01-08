e6704ade52bc5c8f79f666adaf158f35a1ea8176
SanderLentink.nl <sander@lent.ink>
2021-01-08 21:38:59 +0100

    added services

--- namespaces/scrape-strooikaart-job/docker-entrypoint.sh
+++ namespaces/scrape-strooikaart-job/docker-entrypoint.sh
@@ -2,8 +2,11 @@
 
 HOUR_IN_NL="`TZ=CET date +%H`"
 # this is a fallback for misconfigured timezone on the server
+
 # the strooikaart dailySaltUsed is reset at 19:00 CET
-if [ "$HOUR_IN_NL" = "18" ]; then
+# But their data is unreliable (not reset at 19:00 but earlier),
+# therefore I'll scrape the seasonalSaltUsed at 23:xx CET
+if [ "$HOUR_IN_NL" = "23" ]; then
   echo "Last hour of the day"
 else
   echo "Not yet the right hour"
@@ -17,6 +20,6 @@ apk add --no-cache curl jq
 echo -n "`date +%F`," >> $OUTPUT
 
 curl --silent https://rijkswaterstaatstrooit.nl/api/statistics \
-  | jq ".dailySaltUsed" \
+  | jq ".seasonalSaltUsed" \
   >> $OUTPUT
 
