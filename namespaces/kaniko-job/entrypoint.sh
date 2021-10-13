265ad1814e2d094b1d31f1a8fdf9087d9bb5bde4
SanderLentink.nl <sander@lent.ink>
2021-10-13 19:13:14 +0200

    WIP

--- namespaces/kaniko-job/entrypoint.sh
+++ namespaces/kaniko-job/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ve
 
-if [ -f /tmp/kaniko/entrypoint ]; then
-	cp /tmp/kaniko/entrypoint /kaniko/.docker/entrypoint
-	echo Entrypoint already there stopping for faster debugging
-	exit 0
-fi
-
 JOBLIST=/tmp/list
 AMOUNT_RUNS_PER_DAY=6
 CURRENT_HOUR=`date +%H`
@@ -27,7 +21,7 @@ cat << EOF > /kaniko/.docker/config.json
 EOF
 
 OFFSET_OF_THE_DAY=$(($AMOUNT_RUNS_PER_DAY * $(($CURRENT_DAY - 1)) ))
-echo "$AMOUNT_RUNS_PER_DAY, $CURRENT_HOUR, $CURRENT_DAY, $TOTAL_JOBS, $OFFSET_OF_THE_DAY"
+#echo "$AMOUNT_RUNS_PER_DAY, $CURRENT_HOUR, $CURRENT_DAY, $TOTAL_JOBS, $OFFSET_OF_THE_DAY"
 LINE_NUMBER=$(( $(( $(($CURRENT_HOUR + $OFFSET_OF_THE_DAY)) % $TOTAL_JOBS )) + 1 ))
 LINE_TO_EXECUTE=`head -$LINE_NUMBER $JOBLIST| tail -1`
 echo "$LINE_NUMBER: $LINE_TO_EXECUTE"
@@ -37,14 +31,15 @@ if [[ -z "$LINE_TO_EXECUTE" ]]; then
 fi
 
 # https://github.com/GoogleContainerTools/kaniko/blob/master/deploy/Dockerfile
-cat << EOF > /tmp/script
-#!/bin/sh
+# https://github.com/GoogleContainerTools/kaniko/issues/475
+cat << EOF > /kaniko/.docker/entrypoint
+#!/busybox/sh
 /kaniko/executor $LINE_TO_EXECUTE
 EOF
-chmod +x /tmp/script
-which shc || (apt update; apt install -y shc gcc)
-shc -r -U -f /tmp/script -o /kaniko/.docker/entrypoint
-cp /kaniko/.docker/entrypoint /tmp/kaniko/entrypoint
+chmod +x /kaniko/.docker/entrypoint
 
+exit 0
+cat /kaniko/.docker/entrypoint
+cat /kaniko/.docker/config.json # WARNING exposes password to logs
 echo "finished $0"
 
